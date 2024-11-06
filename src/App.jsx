61bfa9e151b93f2229a3a60f665efd176d616a94
Kieran O'Callaghan <123952581+KieranOCallaghan-cork@users.noreply.github.com>
2024-11-06 23:11:23 +0000

    Nav Filled Out

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './componants/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,12 @@ function App() {
     <>
 
       <div>
+        <Route>
           <Navbar />
+            <Switch>
+              <Route path='/' exact />
+            </Switch>
+          </Route>
 
 
         <a href="https://vite.dev" target="_blank">
@@ -33,6 +39,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
     </>
   )
 }
