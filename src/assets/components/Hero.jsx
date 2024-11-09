535360df5e8892db2e4d80c893467d45cbf7e5be
Blaithnaid <iarlaken@gmail.com>
2024-11-09 16:29:50 +0000

    update hero styling a little

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -5,17 +5,17 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-	{ name: "Product", href: "#" },
+	{ name: "About", href: "#" },
 	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" },
+	{ name: "Platforms", href: "#" },
+	{ name: "Download", href: "#" },
 ];
 
 export default function Hero() {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<div className="bg-white">
+		<div className="bg-white h-full w-full">
 			<header className="absolute inset-x-0 top-0 z-50">
 				<nav
 					aria-label="Global"
@@ -130,7 +130,7 @@ export default function Hero() {
 					/>
 				</div>
 				<div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-					<div className="hidden sm:mb-8 sm:flex sm:justify-center">
+					{/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
 						<div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
 							Announcing our next round of funding.{" "}
 							<a
@@ -144,7 +144,7 @@ export default function Hero() {
 								Read more <span aria-hidden="true">&rarr;</span>
 							</a>
 						</div>
-					</div>
+					</div> */}
 					<div className="text-center">
 						<h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
 							Data to enrich your online business
@@ -159,7 +159,7 @@ export default function Hero() {
 								href="#"
 								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 							>
-								Get started
+								Download
 							</a>
 							<a
 								href="#"
