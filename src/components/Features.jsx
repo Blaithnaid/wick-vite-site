4bddc584e76ce1ec7139f79a5db350586ea33a50
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2024-11-09 17:57:02 +0000

    adding description of features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,33 @@
+import {
+	UserCircleIcon,
+	CloudArrowUpIcon,
+	FingerPrintIcon,
+	LockClosedIcon,
+} from "@heroicons/react/24/outline";
+
+const features = [
+	{
+		name: "",
+		description: "",
+		icon: UserCircleIcon,
+	},
+	{
+		name: "",
+		description: "",
+		icon: UserCircleIcon,
+	},
+	{
+		name: "",
+		description: "",
+		icon: UserCircleIcon,
+	},
+	{
+		name: "",
+		description: "",
+		icon: UserCircleIcon,
+	},
+];
+
 const Features = () => {
 	return <div></div>;
 };
