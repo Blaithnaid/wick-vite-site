d0ab1f4c270ac267ed1ecfe7d311b04e86e52187
Kieran O'Callaghan <123952581+KieranOCallaghan-cork@users.noreply.github.com>
2024-11-17 17:32:45 +0000

    Nav Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,17 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+// src/components/Navbar.jsx
+import React from 'react';
 
-function Navbar () {
-const [click, setClick] = useState(false);
-
-const handleClick = () => setClick(!click);
-const closeMenu= () => setClick(false);
+const Navbar = () => {
   return (
-   <>
-    <nav className="navbar">
-        <div className="navbar-container">
-            <link to="/" className='navbar-logo'>
-            
-            </link>
-                <div className='menu-icon' onClick={handleClick}>
-                
-                </div>
-
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                   <li className='nav-item'>
-                    <link to='/' className='nav-links' onClick={closeMenu}>
-                    Home
-                    </link>
-                   </li>
-                   <li className='nav-item'>
-                    <link to='/about' className='nav-links' onClick={closeMenu}>
-                    About
-                    </link>
-                   </li>
-                   <li className='nav-item'>
-                    <link to='/shop' className='nav-links' onClick={closeMenu}>
-                    Shop
-                    </link>
-                   </li>
-                   <li className='nav-item'>
-                    <link to='/login' className='nav-links-mobile' onClick={closeMenu}>
-                    Login
-                    </link>
-                   </li>
-                </ul>
-        </div>
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+      <h1>React Navbar</h1>
+      <ul style={{ listStyleType: 'none', display: 'flex', gap: '10px' }}>
+        <li><a href="#home" style={{ color: '#fff', textDecoration: 'none' }}>Home</a></li>
+        <li><a href="#about" style={{ color: '#fff', textDecoration: 'none' }}>About</a></li>
+        <li><a href="#contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</a></li>
+      </ul>
     </nav>
-   </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
