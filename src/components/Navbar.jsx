5343fc33ec27546cd128691c30e8c7cfd9326d1b
Kieran O'Callaghan <123952581+KieranOCallaghan-cork@users.noreply.github.com>
2024-11-17 17:28:14 +0000

    Nav Aadded to code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar () {
+const [click, setClick] = useState(false);
+
+const handleClick = () => setClick(!click);
+const closeMenu= () => setClick(false);
+  return (
+   <>
+    <nav className="navbar">
+        <div className="navbar-container">
+            <link to="/" className='navbar-logo'>
+            
+            </link>
+                <div className='menu-icon' onClick={handleClick}>
+                
+                </div>
+
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                   <li className='nav-item'>
+                    <link to='/' className='nav-links' onClick={closeMenu}>
+                    Home
+                    </link>
+                   </li>
+                   <li className='nav-item'>
+                    <link to='/about' className='nav-links' onClick={closeMenu}>
+                    About
+                    </link>
+                   </li>
+                   <li className='nav-item'>
+                    <link to='/shop' className='nav-links' onClick={closeMenu}>
+                    Shop
+                    </link>
+                   </li>
+                   <li className='nav-item'>
+                    <link to='/login' className='nav-links-mobile' onClick={closeMenu}>
+                    Login
+                    </link>
+                   </li>
+                </ul>
+        </div>
+    </nav>
+   </>
+  )
+}
+
+export default Navbar
