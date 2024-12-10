e4fc1ecdfdf79835637818c6f583f19b23e0dc31
Blaithnaid <iarlaken@gmail.com>
2024-12-10 21:29:37 +0000

    setting up for firebase integration

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-
-import {  initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getfirestrore } from "firebase/firestore";
 
-
-const firebaseConfig = {    
-    apiKey: "AIzaSyD1J9Q",
-    authDomain: "myapp.firebaseapp.com",
-    projectId: "myapp",
-    storageBucket: "myapp.appspot.com",
-    messagingSenderId: "12345",
-    appId: "1:12345:web:6789",
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
