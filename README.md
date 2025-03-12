d1a6b7283a968c36ee682a7ab62c5f4ee9214c11
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-03-12 14:57:55 +0000

    remove unused navbar component, update readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# React + Vite
+# wick website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+to run the site:
 
-Currently, two official plugins are available:
+- install all dependencies with `npm install`
+- run the dev server `npx vite`
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+to deploy the site:
+
+- install the firebase CLI with `npm install -g firebase-tools`
+- log in with `firebase login`
+- deploy the site with `firebase deploy --only hosting`
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import React from "react";
-import ReactDOM from "react-dom";
 import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Navigate,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
 } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Shop from "./pages/Shop";
 import Login from "./pages/Login";
 import SignIn from "./pages/SignIn";
@@ -17,27 +14,27 @@ import "./App.css";
 import Home from "./pages/Home";
 
 const About = () => (
-	<div style={{ padding: "20px" }}>
-		<h2>About Us</h2>
-		<p>This is the About Page.</p>
-	</div>
+  <div style={{ padding: "20px" }}>
+    <h2>About Us</h2>
+    <p>This is the About Page.</p>
+  </div>
 );
 
 const App = () => {
-	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Navigate to="/home" replace />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/shop" element={<Shop />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signIn" element={<SignIn />} />
-				<Route path="/downloadPage" element={<DownloadPage />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/downloadPage" element={<DownloadPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
