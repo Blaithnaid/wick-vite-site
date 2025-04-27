733effb2eb949a0d288d94f3a6b8f598a3eca486
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-04-28 00:13:11 +0100

    rework of shop

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import SignIn from "./pages/SignIn";
 import DownloadPage from "./pages/DownloadPage";
 import LoggedinPage from "./pages/LoggedinPage";
 import Account from "./pages/Account";
-import Shop2 from "./pages/Shop2";
 
 import "./App.css";
 
@@ -28,7 +27,6 @@ const App = () => {
 				<Route path="/downloadPage" element={<DownloadPage />} />
 				<Route path="/loggedinPage" element={<LoggedinPage />} />
 				<Route path="/account" element={<Account />} />
-				<Route path="/shop2" element={<Shop2 />} />
 			</Routes>
 		</Router>
 	);
