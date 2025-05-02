12a44736b4dcf7492bc8d3bbbd57c7be5e5b1534
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-05-02 23:02:08 +0100

    remove firebase details

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBHuRAOylspqxJ_EIMnVG5AoNtRoNp86PU",
-	authDomain: "wick-232f0.firebaseapp.com",
-	projectId: "wick-232f0",
-	storageBucket: "wick-232f0.firebasestorage.app",
-	messagingSenderId: "73405681834",
-	appId: "1:73405681834:web:7b7f6bc9a7c7219a7b40d3",
-	measurementId: "G-EL9Z718383",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
